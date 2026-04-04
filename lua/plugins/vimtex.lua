d13651a3e29607681b2e3363b11caab1809d7f9e
Yan Moiseev <frenzy4way@gmail.com>
2026-04-04 12:14:44 +0300

    update(vimtex): setup previewer config

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,6 +6,10 @@ return {
 		vim.g.vimtex_compiler_latexmk_engines = {
 			_ = "-lualatex"
 		}
+
+		vim.g.vimtex_view_automatic = 0
+		vim.g.vimtex_view_method = "general"
+		vim.g.vimtex_view_general_viewer = 'okular'
 	end,
 	--config = function() require("vimtex").setup {} end,
 }
